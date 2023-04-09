5182ef8e1f492baddd5bc877a07f769d6532384c
Luke Harrison <lharriso@nyit.edu>
2023-04-09 12:36:33 -0400

    chore: added animations

--- src/components/GaphNode/GaphNode.tsx
+++ src/components/GaphNode/GaphNode.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SimulationGaphNode } from '../../types/d3-force'
 import { useAppDispatch } from '../../hooks'
 import { pushNode } from '../Game/gameSlice'
+import { useSpringRef, useSpring, animated, SpringConfig } from 'react-spring'
 
 interface GaphNodeProps {
   node: SimulationGaphNode
@@ -11,12 +12,8 @@ interface GaphNodeProps {
 export default function GaphNode(props: GaphNodeProps) {
   const dispatch = useAppDispatch()
 
-  const handleNodeClick = () => {
-    dispatch(pushNode(props.node.id))
-  }
-
-  const getNodeColor = () => {
-    switch (props.node.clickedCount) {
+  const getNodeColor = (clickedCount: number) => {
+    switch (clickedCount) {
       case 0:
         return 'grey'
       case 1:
@@ -28,9 +25,26 @@ export default function GaphNode(props: GaphNodeProps) {
     }
   }
 
+  const api = useSpringRef()
+  const config: SpringConfig = { duration: 150 }
+  const circleProps = useSpring({
+    ref: api,
+    from: { r: props.radius, fill: getNodeColor(props.node.clickedCount - 1) },
+    to: [
+      { r: props.radius * 0.75, fill: getNodeColor(props.node.clickedCount) },
+      { r: props.radius, fill: getNodeColor(props.node.clickedCount) },
+    ],
+    config: config,
+  })
+
+  const handleNodeClick = () => {
+    dispatch(pushNode(props.node.id))
+    api.start()
+  }
+
   return (
     <g onClick={handleNodeClick}>
-      <circle cx={props.node.x} cy={props.node.y} r={props.radius} fill={getNodeColor()} />
+      <animated.circle cx={props.node.x} cy={props.node.y} r={circleProps.r} fill={circleProps.fill} />
       <text x={props.node.x} y={props.node.y} textAnchor="middle" dominantBaseline="middle" fill="white">
         {props.node.id}
       </text>
